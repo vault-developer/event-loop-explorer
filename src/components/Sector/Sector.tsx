a28c00646a2caa133c5fd9cd76033396b2e1f0f5
Albert Trott <trott.alik@gmail.com>
2024-08-04 19:24:07 +0200

    feat: add css variables

--- src/components/Sector/Sector.tsx
+++ src/components/Sector/Sector.tsx
@@ -2,9 +2,7 @@ import styles from './Sector.module.css';
 
 function Sector() {
   return (
-    // <div style={{position: "relative"}}>
       <div className={styles.sector}/>
-    // </div>
   );
 }
 
