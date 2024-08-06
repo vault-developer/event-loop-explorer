dd1e6ca8afa3c05d1becdca82a1aa819eba48c83
Albert Trott <trott.alik@gmail.com>
2024-08-06 09:03:38 +0200

    refactor: add constants

--- src/components/EventLoop.tsx
+++ src/components/EventLoop.tsx
@@ -4,6 +4,7 @@ import CircleInner from "./CircleInner/CircleInner.tsx";
 import Sector from "./Sector/Sector.tsx";
 import Pointer from "./Pointer/Pointer.tsx";
 import {useEventLoopState} from "../store/store.ts";
+import {EVENT_LOOP_SECTORS_POSITION_DEGREE} from "../constants.ts";
 
 function EventLoop() {
   const render = useEventLoopState(state => state.render);
@@ -14,13 +15,15 @@ function EventLoop() {
       <p>Event Loop</p>
       <Container>
         <CircleOuter/>
-        <Sector colorVar='--circle-disabled-microtask' degree={340}/>
-        <Sector colorVar={colorVarRender} degree={10}/>
-        <Sector colorVar='--circle-disabled-microtask' degree={40}/>
 
-        <Sector colorVar='--circle-disabled-microtask' degree={220}/>
-        <Sector colorVar='--circle-disabled-task' degree={190}/>
-        <Sector colorVar='--circle-disabled-microtask' degree={160}/>
+        <Sector colorVar='--circle-disabled-microtask' degree={EVENT_LOOP_SECTORS_POSITION_DEGREE.microtask4}/>
+        <Sector colorVar={colorVarRender} degree={EVENT_LOOP_SECTORS_POSITION_DEGREE.render}/>
+        <Sector colorVar='--circle-disabled-microtask' degree={EVENT_LOOP_SECTORS_POSITION_DEGREE.microtask1}/>
+
+        <Sector colorVar='--circle-disabled-microtask' degree={EVENT_LOOP_SECTORS_POSITION_DEGREE.microtask2}/>
+        <Sector colorVar='--circle-disabled-task' degree={EVENT_LOOP_SECTORS_POSITION_DEGREE.task}/>
+        <Sector colorVar='--circle-disabled-microtask' degree={EVENT_LOOP_SECTORS_POSITION_DEGREE.microtask3}/>
+
         <Pointer/>
         <CircleInner/>
       </Container>
--- src/components/Pointer/Pointer.tsx
+++ src/components/Pointer/Pointer.tsx
@@ -1,6 +1,9 @@
 import styles from './Pointer.module.css';
 import {useEffect, useRef} from "react";
 import {useEventLoopMutatedState, useEventLoopState} from "../../store/store.ts";
+import {EVENT_LOOP_INNER_SECTOR_OFFSET, EVENT_LOOP_SECTORS_POSITION_DEGREE} from "../../constants.ts";
+
+const renderSectorPosition = EVENT_LOOP_SECTORS_POSITION_DEGREE.render + EVENT_LOOP_INNER_SECTOR_OFFSET;
 
 function Pointer() {
   const eventLoopMutatedState = useEventLoopMutatedState(state => state);
@@ -13,12 +16,12 @@ function Pointer() {
   useEffect(() => {
     const animate = async () => {
       if (sectorInnerRef.current && sectorOuterRef.current) {
-        if (eventLoopMutatedState.render && angle === 10.5) {
+        if (eventLoopMutatedState.render && angle === renderSectorPosition) {
           await new Promise(resolve => setTimeout(resolve, 3000));
           setRender(false);
         }
 
-        sectorInnerRef.current.style.transform = `rotate(${angle-0.5}deg)`;
+        sectorInnerRef.current.style.transform = `rotate(${angle-EVENT_LOOP_INNER_SECTOR_OFFSET}deg)`;
         sectorOuterRef.current.style.transform = `rotate(${angle}deg)`;
         angle += 0.5;
         if (angle > 360) angle-=360;
