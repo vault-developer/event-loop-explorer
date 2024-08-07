d746d4c6c3ba6a08202cc2d515be77b8676b2c13
Albert Trott <trott.alik@gmail.com>
2024-08-07 18:44:58 +0200

    feat: simplify labels

--- src/components/CircleLabels/CircleLabels.tsx
+++ src/components/CircleLabels/CircleLabels.tsx
@@ -1,76 +1,48 @@
 import styles from './CircleLabels.module.css';
 import {EVENT_LOOP_SECTORS_POSITION_DEGREE} from "../../constants.ts";
 
-function CircleLabels() {
-  const realRenderDegree = (360 - (EVENT_LOOP_SECTORS_POSITION_DEGREE.render - 10)) % 360;
+const entitiesOldDegree = {
+  render: EVENT_LOOP_SECTORS_POSITION_DEGREE.render,
+  task: EVENT_LOOP_SECTORS_POSITION_DEGREE.task,
+  microtask0: EVENT_LOOP_SECTORS_POSITION_DEGREE.microtasks[0],
+  microtask1: EVENT_LOOP_SECTORS_POSITION_DEGREE.microtasks[1],
+  microtask2: EVENT_LOOP_SECTORS_POSITION_DEGREE.microtasks[2],
+  microtask3: EVENT_LOOP_SECTORS_POSITION_DEGREE.microtasks[3],
+};
+
+const titles = {
+  render: 'R',
+  task: 'T',
+  microtask0: 'MT',
+  microtask1: 'MT',
+  microtask2: 'MT',
+  microtask3: 'MT',
+};
+
+const data = Object.entries(entitiesOldDegree).reduce((acc, [key, value]) => {
+  const realRenderDegree = (360 - (value - 10)) % 360;
   const renderCos = Math.cos(realRenderDegree * Math.PI / 180);
   const renderSin = Math.sin(realRenderDegree * Math.PI / 180);
+  acc.push({
+    title: titles[key as keyof typeof titles],
+    cos: renderCos,
+    sin: renderSin,
+  });
+  return acc;
+}, [] as Array<{ title: string, cos: number, sin: number }>);
 
-  const realRenderTask = (360 - (EVENT_LOOP_SECTORS_POSITION_DEGREE.task - 10)) % 360;
-  const taskCos = Math.cos(realRenderTask * Math.PI / 180);
-  const taskSin = Math.sin(realRenderTask * Math.PI / 180);
-
-  const realRenderMicroTask0 = (360 - (EVENT_LOOP_SECTORS_POSITION_DEGREE.microtasks[0] - 10)) % 360;
-  const microTaskCos0 = Math.cos(realRenderMicroTask0 * Math.PI / 180);
-  const microTaskSin0 = Math.sin(realRenderMicroTask0 * Math.PI / 180);
-
-  const realRenderMicroTask1 = (360 - (EVENT_LOOP_SECTORS_POSITION_DEGREE.microtasks[1] - 10)) % 360;
-  const microTaskCos1 = Math.cos(realRenderMicroTask1 * Math.PI / 180);
-  const microTaskSin1 = Math.sin(realRenderMicroTask1 * Math.PI / 180);
-
-  const realRenderMicroTask2 = (360 - (EVENT_LOOP_SECTORS_POSITION_DEGREE.microtasks[2] - 10)) % 360;
-  const microTaskCos2 = Math.cos(realRenderMicroTask2 * Math.PI / 180);
-  const microTaskSin2 = Math.sin(realRenderMicroTask2 * Math.PI / 180);
-
-  const realRenderMicroTask3 = (360 - (EVENT_LOOP_SECTORS_POSITION_DEGREE.microtasks[3] - 10)) % 360;
-  const microTaskCos3 = Math.cos(realRenderMicroTask3 * Math.PI / 180);
-  const microTaskSin3 = Math.sin(realRenderMicroTask3 * Math.PI / 180);
-
+function CircleLabels() {
   return (
     <div className={styles.circleLabels}>
-      <p style={{
-        position: "absolute",
-        left: `${300 + renderCos * (300 - 25)}px`,
-        top: `${300 + renderSin * (300 - 25)}px`,
-        margin: 0,
-        transform: 'translate(-50%, -50%)',
-      }}>R</p>
-      <p style={{
-        position: "absolute",
-        left: `${300 + taskCos * (300 - 25)}px`,
-        top: `${300 + taskSin * (300 - 25)}px`,
-        margin: 0,
-        transform: 'translate(-50%, -50%)',
-      }}>T</p>
-      <p style={{
-        position: "absolute",
-        left: `${300 + microTaskCos0 * (300 - 25)}px`,
-        top: `${300 + microTaskSin0 * (300 - 25)}px`,
-        margin: 0,
-        transform: 'translate(-50%, -50%)',
-      }}>M</p>
-      <p style={{
-        position: "absolute",
-        left: `${300 + microTaskCos1 * (300 - 25)}px`,
-        top: `${300 + microTaskSin1 * (300 - 25)}px`,
-        margin: 0,
-        transform: 'translate(-50%, -50%)',
-      }}>M</p>
-      <p style={{
-        position: "absolute",
-        left: `${300 + microTaskCos2 * (300 - 25)}px`,
-        top: `${300 + microTaskSin2 * (300 - 25)}px`,
-        margin: 0,
-        transform: 'translate(-50%, -50%)',
-      }}>M</p>
-      <p style={{
-        position: "absolute",
-        left: `${300 + microTaskCos3 * (300 - 25)}px`,
-        top: `${300 + microTaskSin3 * (300 - 25)}px`,
-        margin: 0,
-        transform: 'translate(-50%, -50%)',
-      }}>M</p>
-
+      {data.map(({title, cos, sin}) => (
+        <p style={{
+          position: "absolute",
+          left: `${300 + cos * (300 - 25)}px`,
+          top: `${300 + sin * (300 - 25)}px`,
+          margin: 0,
+          transform: 'translate(-50%, -50%)',
+        }}>{title}</p>
+      ))}
     </div>
   );
 }
