819217ce71029529c6ab63a05f12d2f9230af68d
Albert Trott <trott.alik@gmail.com>
2024-08-07 19:09:06 +0200

    feat: add events data

--- src/components/CircleLabels/CircleLabels.tsx
+++ src/components/CircleLabels/CircleLabels.tsx
@@ -1,44 +1,41 @@
 import styles from './CircleLabels.module.css';
 import {EVENT_LOOP_SECTORS_POSITION_DEGREE} from "../../constants.ts";
 
-const entitiesOldDegree = {
-  render: EVENT_LOOP_SECTORS_POSITION_DEGREE.render,
-  task: EVENT_LOOP_SECTORS_POSITION_DEGREE.task,
-  microtask0: EVENT_LOOP_SECTORS_POSITION_DEGREE.microtasks[0],
-  microtask1: EVENT_LOOP_SECTORS_POSITION_DEGREE.microtasks[1],
-  microtask2: EVENT_LOOP_SECTORS_POSITION_DEGREE.microtasks[2],
-  microtask3: EVENT_LOOP_SECTORS_POSITION_DEGREE.microtasks[3],
-};
+interface Event {
+  title: string;
+  type: 'task' | 'microtask' | 'render';
+  degree: number;
+}
 
-const titles = {
-  render: 'R',
-  task: 'T',
-  microtask0: 'MT',
-  microtask1: 'MT',
-  microtask2: 'MT',
-  microtask3: 'MT',
-};
+const events: Event[] = [
+  {title: 'R', type: 'render', degree: (360 - (EVENT_LOOP_SECTORS_POSITION_DEGREE.render - 10)) % 360},
+  {title: 'T', type: 'task', degree: (360 - (EVENT_LOOP_SECTORS_POSITION_DEGREE.task - 10)) % 360},
+  {title: 'MT', type: 'microtask', degree: (360 - (EVENT_LOOP_SECTORS_POSITION_DEGREE.microtasks[0] - 10)) % 360},
+  {title: 'MT', type: 'microtask', degree: (360 - (EVENT_LOOP_SECTORS_POSITION_DEGREE.microtasks[1] - 10)) % 360},
+  {title: 'MT', type: 'microtask', degree: (360 - (EVENT_LOOP_SECTORS_POSITION_DEGREE.microtasks[2] - 10)) % 360},
+  {title: 'MT', type: 'microtask', degree: (360 - (EVENT_LOOP_SECTORS_POSITION_DEGREE.microtasks[3] - 10)) % 360},
+];
 
-const data = Object.entries(entitiesOldDegree).reduce((acc, [key, value]) => {
-  const realRenderDegree = (360 - (value - 10)) % 360;
-  const renderCos = Math.cos(realRenderDegree * Math.PI / 180);
-  const renderSin = Math.sin(realRenderDegree * Math.PI / 180);
-  acc.push({
-    title: titles[key as keyof typeof titles],
-    cos: renderCos,
-    sin: renderSin,
-  });
-  return acc;
-}, [] as Array<{ title: string, cos: number, sin: number }>);
+const enhancedEvents = events.map(event => {
+  const cos = Math.cos(event.degree * Math.PI / 180);
+  const sin = Math.sin(event.degree * Math.PI / 180);
+  const leftOffset = 300 + cos * (300 - 25);
+  const topOffset = 300 + sin * (300 - 25);
+  return {
+    ...event,
+    leftOffset,
+    topOffset,
+  };
+});
 
 function CircleLabels() {
   return (
     <div className={styles.circleLabels}>
-      {data.map(({title, cos, sin}) => (
+      {enhancedEvents.map(({title, leftOffset, topOffset}) => (
         <p style={{
           position: "absolute",
-          left: `${300 + cos * (300 - 25)}px`,
-          top: `${300 + sin * (300 - 25)}px`,
+          left: `${leftOffset}px`,
+          top: `${topOffset}px`,
           margin: 0,
           transform: 'translate(-50%, -50%)',
         }}>{title}</p>
