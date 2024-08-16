36ba0104b3ddf2725e308040c6f507d92d7b1988
Albert Trott <trott.alik@gmail.com>
2024-08-16 09:00:12 +0200

    refactor

--- src/utils/nodes/MemberExpression.ts
+++ src/utils/nodes/MemberExpression.ts
@@ -1,4 +1,4 @@
-import {MemberExpression} from "acorn";
+import {CallExpression, MemberExpression} from "acorn";
 import {NodeClass, NodeClassConstructor} from "./Node.abstract.ts";
 import {nodeFactory} from "./factory.ts";
 
@@ -9,34 +9,67 @@ export class MemberExpressionClass extends NodeClass {
 
   serialize = () => {
     const expression = this.node as MemberExpression;
-    if (expression.object.type !== 'Identifier') {
-      console.log('Serialize:MemberExpression:Identifier object.type is supported', expression);
-      return '';
-    } else if (expression.property.type !== 'Identifier') {
-      console.log('Serialize:MemberExpression:Identifier property.type is supported', expression);
-      return '';
-    }
 
-    return `${expression.object.name}.${expression.property.name}`;
+    const object = nodeFactory({
+      node: expression.object,
+      context: this.context,
+      args: this.args,
+      params: this.params,
+    }).serialize();
+
+    const property = nodeFactory({
+      node: expression.property,
+      context: this.context,
+      args: this.args,
+      params: this.params,
+    }).serialize();
+
+    return `${object}.${property}`;
   }
 
   traverse = () => {
-    const expression = this.node as MemberExpression;
-    if (expression.object.type !== 'Identifier') {
-      return console.log('Traverse:MemberExpression: object.type: only Identifier is supported', expression);
-    }
-    if (expression.object.name !== 'console') {
-      return console.log('Traverse:MemberExpression: object.name: only console is supported', expression);
+    const memberExpression = this.node as MemberExpression;
+
+    // handle console
+    if (memberExpression.object.type === 'Identifier' && memberExpression.object.name === 'console') {
+      return this.context.steps.push({
+        sector: 'console',
+        action: 'push',
+        value: this.args?.map(arg => nodeFactory({
+          node: arg,
+          context: this.context,
+          params: this.params,
+        }).serialize()).join(',') ?? ''
+      });
     }
 
-    this.context.steps.push({
-      sector: 'console',
-      action: 'push',
-      value: this.args?.map(arg => nodeFactory({
-        node: arg,
-        context: this.context,
-        params: this.params,
-      }).serialize()).join(',') ?? ''
-    });
+    // handle promise.resolve().then()
+    if (
+      memberExpression.object.type === 'CallExpression' &&
+      memberExpression.property.type === 'Identifier' &&
+      memberExpression.property.name === 'then'
+    ) {
+      const callExpression = memberExpression.object as CallExpression;
+      if (callExpression.callee.type === 'MemberExpression') {
+        const innerMemberExpression = callExpression.callee;
+        if (
+          innerMemberExpression.object.type === 'Identifier' &&
+          innerMemberExpression.object.name === 'Promise'
+        ) {
+          if (
+            innerMemberExpression.property.type === 'Identifier' &&
+            innerMemberExpression.property.name === 'resolve'
+          ) {
+            return this.context.steps.push({
+              sector: 'microtask_queue',
+              action: 'push',
+              value: this.args?.[0]
+            });
+          }
+        }
+      }
+    }
+
+    return console.log('Traverse:MemberExpression: not supported', memberExpression);
   }
 }
