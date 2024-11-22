3544005c440746c615279c4d2ed1b5677745f0b6
Raj <raj.kumar.s.dev@gmail.com>
2024-11-22 13:39:35 +0530

    refactor: inverted media queries from max width to min width (#24)

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -4,15 +4,15 @@ import styled from '@emotion/styled';
 export const Layout = styled.div`
 	flex: 1;
 	display: grid;
-	grid-template-columns: minmax(0, 3fr) minmax(0, 1fr) minmax(0, 3fr);
-	grid-template-rows: repeat(36, 1fr);
-	grid-gap: 20px;
 	padding: 20px;
+	grid-template-columns: 1fr;
+	grid-template-rows: auto;
+	grid-gap: 10px;
 
-	@media (max-width: 768px) {
-		grid-template-columns: 1fr;
-		grid-template-rows: auto;
-		grid-gap: 10px;
+	@media (min-width: 768px) {
+		grid-template-columns: minmax(0, 3fr) minmax(0, 1fr) minmax(0, 3fr);
+		grid-template-rows: repeat(36, 1fr);
+		grid-gap: 20px;
 	}
 `;
 
@@ -35,99 +35,104 @@ const List = styled(BaseLayoutElement)`
 `;
 
 export const Info = styled.div`
-	grid-column: 1 / 2;
-	grid-row: 1 / 5;
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
+	grid-column: 1;
+	grid-row: 1;
 
-	@media (max-width: 768px) {
-		grid-column: 1;
-		grid-row: 1;
+	@media (min-width: 768px) {
+		grid-column: 1 / 2;
+		grid-row: 1 / 5;
 	}
 `;
 
 export const Editor = styled(BaseLayoutElement)`
 	padding: 0;
-	grid-column: 1 / 2;
-	grid-row: 5 / 31;
+	grid-column: 1;
+	grid-row: 2;
+	height: 50vh;
 
-	@media (max-width: 768px) {
-		grid-column: 1;
-		grid-row: 2;
-		height: 50vh;
+	@media (min-width: 768px) {
+		grid-column: 1 / 2;
+		grid-row: 5 / 31;
+		height: unset;
 	}
 `;
 
 export const WebApi = styled(List)`
-	grid-column: 1 / 2;
-	grid-row: 31 / 37;
+	grid-column: 1;
+	grid-row: 3;
+	height: 10vh;
 
-	@media (max-width: 768px) {
-		grid-column: 1;
-		grid-row: 3;
-		height: 10vh;
+	@media (min-width: 768px) {
+		grid-column: 1 / 2;
+		grid-row: 31 / 37;
+		height: unset;
 	}
 `;
 
 export const CallStack = styled(List)`
-	grid-column: 2 / 3;
-	grid-row: 1 / 19;
+	grid-column: 1;
+	grid-row: 4;
+	height: 10vh;
 
-	@media (max-width: 768px) {
-		grid-column: 1;
-		grid-row: 4;
-		height: 10vh;
+	@media (min-width: 768px) {
+		grid-column: 2 / 3;
+		grid-row: 1 / 19;
+		height: unset;
 	}
 `;
 
 export const Console = styled(List)`
-	grid-column: 2 / 3;
-	grid-row: 19 / 37;
+	grid-column: 1;
+	grid-row: 5;
+	height: 10vh;
 
-	@media (max-width: 768px) {
-		grid-column: 1;
-		grid-row: 5;
-		height: 10vh;
+	@media (min-width: 768px) {
+		grid-column: 2 / 3;
+		grid-row: 19 / 37;
+		height: unset;
 	}
 `;
 
 export const RenderCallbacks = styled(List)`
-	grid-column: 3 / 4;
-	grid-row: 1 / 7;
+	grid-column: 1;
+	grid-row: 6;
+	height: 10vh;
 
-	@media (max-width: 768px) {
-		grid-column: 1;
-		grid-row: 6;
-		height: 10vh;
+	@media (min-width: 768px) {
+		grid-column: 3 / 4;
+		grid-row: 1 / 7;
+		height: unset;
 	}
 `;
 
 export const Tasks = styled(List)`
-	grid-column: 3 / 4;
-	grid-row: 7 / 13;
+	grid-column: 1;
+	grid-row: 7;
+	height: 10vh;
 
-	@media (max-width: 768px) {
-		grid-column: 1;
-		grid-row: 7;
-		height: 10vh;
+	@media (min-width: 768px) {
+		grid-column: 3 / 4;
+		grid-row: 7 / 13;
+		height: unset;
 	}
 `;
 
 export const Microtasks = styled(List)`
-	grid-column: 3 / 4;
-	grid-row: 13 / 19;
+	grid-column: 1;
+	grid-row: 8;
+	height: 10vh;
 
-	@media (max-width: 768px) {
-		grid-column: 1;
-		grid-row: 8;
-		height: 10vh;
+	@media (min-width: 768px) {
+		grid-column: 3 / 4;
+		grid-row: 13 / 19;
+		height: unset;
 	}
 `;
 
 export const EventLoop = styled(BaseLayoutElement)`
-	grid-column: 3 / 4;
-	grid-row: 19 / 37;
 	display: flex;
 	flex-direction: column;
 	justify-content: space-between;
@@ -135,11 +140,14 @@ export const EventLoop = styled(BaseLayoutElement)`
 	overflow: auto;
 	gap: 10px;
 	position: relative;
+	grid-column: 1;
+	grid-row: 9;
+	height: 40vh;
 
-	@media (max-width: 768px) {
-		grid-column: 1;
-		grid-row: 9;
-		height: 40vh;
+	@media (min-width: 768px) {
+		grid-column: 3 / 4;
+		grid-row: 19 / 37;
+		height: unset;
 	}
 `;
 
