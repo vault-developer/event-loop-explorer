09ef6c66be7831811fba520f63d3acbf6c4dfae0
Albert Trott <trott.alik@gmail.com>
2024-12-26 01:07:23 +0100

    refactor: simplify utils

--- src/v2-architecture/ast.utils.ts
+++ src/v2-architecture/ast.utils.ts
@@ -1,41 +1,39 @@
-import {CallExpression} from 'acorn';
-import {isCallExpression, isIdentifier, isMemberExpression} from './ast.guards.ts';
+import { CallExpression } from 'acorn';
+import {
+	isCallExpression,
+	isIdentifier,
+	isMemberExpression,
+} from './ast.guards.ts';
 
 export function isConsoleExpression(expression: CallExpression): boolean {
 	const callee = expression.callee;
-	if (isMemberExpression(callee) !== true) return false;
-	const {object, property} = callee;
-	if (isIdentifier(object) !== true) return false;
-	if (isIdentifier(property) !== true) return false;
-
 	return (
-		object.name === 'console' &&
-		['log', 'info', 'error', 'warn'].includes(property.name)
+		isMemberExpression(callee) &&
+		isIdentifier(callee.object) &&
+		isIdentifier(callee.property) &&
+		callee.object.name === 'console' &&
+		['log', 'info', 'error', 'warn'].includes(callee.property.name)
 	);
 }
 
 export function isSetTimeoutExpression(expression: CallExpression): boolean {
 	const callee = expression.callee;
-	if (isIdentifier(callee) !== true) return false;
-
-	return callee.name === 'setTimeout';
+	return isIdentifier(callee) && callee.name === 'setTimeout';
 }
 
-export function isPromiseCallbackExpression(expression: CallExpression): boolean {
+export function isPromiseCallbackExpression(
+	expression: CallExpression
+): boolean {
 	const callee = expression.callee;
-	if (isMemberExpression(callee) !== true) return false;
-	const {object: calleeCallExpression, property: calleeProperty} = callee;
-	if (isCallExpression(calleeCallExpression) !== true) return false;
-	if (isIdentifier(calleeProperty) !== true) return false;
-	const innerCalleMemberExpression = calleeCallExpression.callee;
-	if (isMemberExpression(innerCalleMemberExpression) !== true) return false;
-	const {object , property } = innerCalleMemberExpression;
-	if (isIdentifier(object) !== true) return false;
-	if (isIdentifier(property) !== true) return false;
-
 	return (
-		object.name === 'Promise' &&
-		property.name === 'resolve' &&
-		calleeProperty.name === 'then'
+		isMemberExpression(callee) &&
+		isIdentifier(callee.property) &&
+		callee.property.name === 'then' &&
+		isCallExpression(callee.object) &&
+		isMemberExpression(callee.object.callee) &&
+		isIdentifier(callee.object.callee.object) &&
+		callee.object.callee.object.name === 'Promise' &&
+		isIdentifier(callee.object.callee.property) &&
+		callee.object.callee.property.name === 'resolve'
 	);
 }
